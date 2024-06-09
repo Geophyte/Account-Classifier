89f5ad4875afc2ed1b95628140a86608f3a01f3e
Kravig <milanw.2001@gmail.com>
2024-06-09 12:48:46 +0200

    cleanup of imports

--- app/app.py
+++ app/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, jsonify
 import joblib
-from werkzeug.utils import secure_filename
-from MockModel import MockModel
 import numpy as np
 import pandas as pd
 import random
